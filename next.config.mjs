78234dd9df1a0ecb68287ec6d67583f3f76f9190
Justice Opara <justiceopara33@gmail.com>
2024-10-28 13:27:07 +0100

    Updated Configuration Using CommonJS

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["res.cloudinary.com"],
-      },
- 
-      typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
-     
-
+  images: {
+      domains: ["res.cloudinary.com"],
+  },
+  typescript: {
+      ignoreBuildErrors: true,
+  },
 };
 
-export default nextConfig;
+// Use module.exports to export the configuration
+module.exports = nextConfig;
