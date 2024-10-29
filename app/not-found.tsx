859ca9394a3fe6b5299102c1b2818e67057fcb59
Justice Opara <justiceopara33@gmail.com>
2024-10-29 19:22:02 +0100

    added the cart folder

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h2 className="text-2xl font-bold mb-4">404 - Page Not Found</h2>
-      <p>The page you&lsquo;re looking for doesn&lsquo;t exist.</p>
-    </div>
-  );
-} 
