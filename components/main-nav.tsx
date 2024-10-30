0dd6d8dbec22a26491579ec1ca68f3818485db37
Justice Opara <justiceopara33@gmail.com>
2024-10-30 17:40:10 +0100

    fixing map 5

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,36 +1,79 @@
+// "use client";
+
+// import { cn } from "@/lib/utils";
+// import { Category } from "@/types";
+// import Link from "next/link";
+// import { usePathname } from "next/navigation";
+
+// import React from "react";
+
+// interface MainNavProps {
+//   data: Category[];
+// }
+// const MainNav: React.FC<MainNavProps> = ({ data }) => {
+//   const pathname = usePathname();
+// console.log(data,"--data maindata")
+//   if(!data || data.length === 0){
+//  return 
+//   }
+//   const routes = data.map((route) => ({
+//     href: `/category/${route.id}`,
+//     label: route.name,
+//     active: pathname === `/category/${route.id}`,
+//   }));
+//   console.log(routes)
+
+//   return (
+//     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6 ">
+//        {routes.map((route) => (
+//         <Link
+//           key={route.href}
+//           href={route.href}
+//           className={cn(
+//             "lg:text-xl text-sm font-medium transition-colors hover:text-black",
+//             route.active ? "text-black" : "text-neutral-500"
+//           )}
+//         >
+//           {route.label}
+//         </Link>
+//       ))} 
+//     </nav>
+//   );
+// };
+
+// export default MainNav;
+
 "use client";
 
 import { cn } from "@/lib/utils";
 import { Category } from "@/types";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface MainNavProps {
   data: Category[];
 }
+
 const MainNav: React.FC<MainNavProps> = ({ data }) => {
   const pathname = usePathname();
-  console.log(data, "--data maindata");
-  const [dataValue, setDataValue] = useState<Category[]>([]);
+  const [routes, setRoutes] = useState<{ href: string; label: string; active: boolean }[]>([]);
 
   useEffect(() => {
-    setDataValue(data);
-  }, [data]);
-  // if (!data || data.length === 0) {
-  //   return null;
-  // }
-  const routes = dataValue.map((route) => ({
-    href: `/category/${route.id}`,
-    label: route.name,
-    active: pathname === `/category/${route.id}`,
-  }));
-  console.log(routes, "");
+    if (data && Array.isArray(data) && data.length > 0) {
+      const newRoutes = data.map((route) => ({
+        href: `/category/${route.id}`,
+        label: route.name,
+        active: pathname === `/category/${route.id}`,
+      }));
+      setRoutes(newRoutes);
+    } else {
+      setRoutes([]); // Clear routes if data is empty or undefined
+    }
+  }, [data, pathname]);
 
   return (
-    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6 ">
+    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
       {routes.map((route) => (
         <Link
           key={route.href}
