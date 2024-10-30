acfe58c9da272ffe3cf9ba52c15b946dbe6b11cb
Justice Opara <justiceopara33@gmail.com>
2024-10-30 17:45:33 +0100

    check check

--- app/(routes)/category/[categoryId]/components/filter.tsx
+++ app/(routes)/category/[categoryId]/components/filter.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-// import Button from "@/components/ui/button";
-// import { cn } from "@/lib/utils";
+import Button from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { Color, Size } from "@/types";
-// import { useRouter, useSearchParams } from "next/navigation";
-// import qs from "query-string";
+import { useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 
 interface FilterProps {
   data: (Size | Color)[];
@@ -12,42 +12,37 @@ interface FilterProps {
   valueKey: string;
 }
 
-const Filter: React.FC<FilterProps> = ({ 
-  // data, 
-  name, 
-  // valueKey 
+const Filter: React.FC<FilterProps> = ({ data, name, valueKey }) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const selectedValue = searchParams.get(valueKey);
 
-}) => {
-  // const searchParams = useSearchParams();
-  // const router = useRouter();
-  // const selectedValue = searchParams.get(valueKey);
+  const onClick = (id: string) => {
+    const current = qs.parse(searchParams.toString());
+    const query = {
+      ...current,
+      [valueKey]: id,
+    };
 
-  // const onClick = (id: string) => {
-  //   const current = qs.parse(searchParams.toString());
-  //   const query = {
-  //     ...current,
-  //     [valueKey]: id,
-  //   };
-
-  //   if (current[valueKey] === id) {
-  //     query[valueKey] = null;
-  //   }
-  //   const url = qs.stringifyUrl(
-  //     {
-  //       url: window.location.href,
-  //       query,
-  //     },
-  //     { skipNull: true }
-  //   );
-  //   router.push(url);
-  // };
+    if (current[valueKey] === id) {
+      query[valueKey] = null;
+    }
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipNull: true }
+    );
+    router.push(url);
+  };
 
   return (
     <div className="mb-8">
       <h3 className="text-lg font-semibold ">{name}</h3>
       <hr className="my-4" />
       <div className="flex flex-wrap gap-2">
-        {/* {data.map((filter) => (
+        {data.map((filter) => (
           <div className="flex items-center" key={filter.id}>
             <Button
               className={cn(
@@ -59,7 +54,7 @@ const Filter: React.FC<FilterProps> = ({
               {filter.name}
             </Button>
           </div>
-        ))} */}
+        ))}
       </div>
     </div>
   );
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,47 +1,4 @@
-// "use client";
 
-// import { cn } from "@/lib/utils";
-// import { Category } from "@/types";
-// import Link from "next/link";
-// import { usePathname } from "next/navigation";
-
-// import React from "react";
-
-// interface MainNavProps {
-//   data: Category[];
-// }
-// const MainNav: React.FC<MainNavProps> = ({ data }) => {
-//   const pathname = usePathname();
-// console.log(data,"--data maindata")
-//   if(!data || data.length === 0){
-//  return 
-//   }
-//   const routes = data.map((route) => ({
-//     href: `/category/${route.id}`,
-//     label: route.name,
-//     active: pathname === `/category/${route.id}`,
-//   }));
-//   console.log(routes)
-
-//   return (
-//     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6 ">
-//        {routes.map((route) => (
-//         <Link
-//           key={route.href}
-//           href={route.href}
-//           className={cn(
-//             "lg:text-xl text-sm font-medium transition-colors hover:text-black",
-//             route.active ? "text-black" : "text-neutral-500"
-//           )}
-//         >
-//           {route.label}
-//         </Link>
-//       ))} 
-//     </nav>
-//   );
-// };
-
-// export default MainNav;
 
 "use client";
 
