5210ffe57831da7732b8862fbbca95d54216c177
Justice Opara <justiceopara33@gmail.com>
2024-11-02 18:51:36 +0100

    fx

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,7 +1,38 @@
+// import { Product } from "@/types";
+// import React from "react";
+// import NoResults from "@/components/ui/no-results";
+// // import ProductCard from "@/components/ui/product-card";
+
+// interface ProductListProps {
+//   title: string;
+//   items: Product[];
+// }
+
+// const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
+//   return (
+//     <div className="space-y-4 ">
+//       <h3 className="font-bold text-3xl">{title}</h3>
+//       {items.length === 0 && <NoResults />}
+//       <div
+//         className="grid grid-col-1 sm:grid-cols-2 
+//       md:grid-cols-3 lg:grid-cols-4 gap-4"
+//       >
+//          {/* {items?.map((item) => (
+//           <ProductCard key={item.id} data={item} />
+//         ))}    */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProductList;
+
+"use client"
+import { useEffect, useState } from "react";
 import { Product } from "@/types";
 import React from "react";
 import NoResults from "@/components/ui/no-results";
-// import ProductCard from "@/components/ui/product-card";
+import ProductCard from "@/components/ui/product-card";
 
 interface ProductListProps {
   title: string;
@@ -9,20 +40,30 @@ interface ProductListProps {
 }
 
 const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
+  const [productItems, setProductItems] = useState<Product[]>([]);
+
+  useEffect(() => {
+    if (items) {
+      setProductItems(items);
+    }
+  }, [items]);
+
   return (
-    <div className="space-y-4 ">
+    <div className="space-y-4">
       <h3 className="font-bold text-3xl">{title}</h3>
-      {items.length === 0 && <NoResults />}
+      {productItems.length === 0 && <NoResults />}
       <div
         className="grid grid-col-1 sm:grid-cols-2 
-      md:grid-cols-3 lg:grid-cols-4 gap-4"
+        md:grid-cols-3 lg:grid-cols-4 gap-4"
       >
-         {/* {items?.map((item) => (
+      
+     {productItems.map((item) => (
           <ProductCard key={item.id} data={item} />
-        ))}    */}
+        ))} 
       </div>
     </div>
   );
 };
 
 export default ProductList;
+
